5a792b9a55b0292278de9e514a1a114c0cc36650
Marvin <marvinraj77@gmail.com>
2024-05-07 12:42:28 +0800

    planned out counting sort algorithm

---
+++ algorithms/sorting/counting_sort.py
@@ -0,0 +1,14 @@
+# counting sort
+
+# - get unsorted array
+# - get the count of each element in the string -- store in a count array
+# - get cummulative value of the count array
+# - mave the values to the right, hence the extra space at index 0 = 0
+# - the values in the count array correspond to the index in the sorted array
+# - if there are similar values in the unsorted array, the value/count in the count array that corresponds to the similar values needs to be added by 1
+
+
+
+
+if __name__ == '__main__':
+    print("hello")
